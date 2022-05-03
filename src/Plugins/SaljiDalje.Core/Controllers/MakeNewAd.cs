edfe981302a7593be0d940cad1b74af7c3f906b5
goransipic <Naranca2019>
2022-05-03 09:55:58 +0200

    added new plugin Component SaljiDalje.Core

---
+++ src/Plugins/SaljiDalje.Core/Controllers/MakeNewAd.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Directory;
+
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace SaljiDalje.Core.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class MakeNewAd : BasePluginController
+    {
+        #region Fields
+
+        private readonly ICountryService _countryService;
+
+        #endregion
+
+        #region Ctor
+
+        public MakeNewAd(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<IActionResult> makeNewAd()
+        {
+            return View("~/Plugins/SaljiDalje.Core/Views/Index.cshtml");
+        }
+
+        #endregion
+    }
+}
