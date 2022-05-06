0a976d1ef91cc1d59e9f74c112d15d46c4d789b1
goransipic <Naranca2019>
2022-05-06 16:10:43 +0200

    ewrrw

---
+++ src/Presentation/Nop.Web/Controllers/SaljiDalje.Core/StepsModel.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Models.Catalog;
+
+namespace SaljiDalje.Core
+{
+   public enum StepsModel
+    {
+        STEP_ONE,
+        STEP_TWO
+    }
+}
