466d84f9d76522d6a34098068ac8a1c2b7dd7a99
goransipic <Naranca2019>
2022-05-28 12:55:58 +0200

    cvcxvcv

---
+++ src/Presentation/Nop.Web/Controllers/SaljiDalje.Core/Controllers/CustomerInfoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Controllers;
+
+namespace SaljiDalje.Core.Controllers;
+
+public class CustomerInfoController : BasePluginController
+{
+    public virtual IActionResult Notification()
+    {
+        return View("~/Plugins/SaljiDalje.Core/Views/Notification.cshtml");
+    }
+    
+    public virtual IActionResult CustomerAds()
+    {
+        return View("~/Plugins/SaljiDalje.Core/Views/CustomerAds.cshtml");
+    }
+    
+}
---
+++ src/Presentation/Nop.Web/Controllers/SaljiDalje.Core/CustomerNavigationItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Nop.Web.Controllers.SaljiDalje.Core;
+
+public enum CustomerNavigationItem
+{
+    Info = 0,
+    ChangePassword = 1,
+    CustomerAds = 2,
+    ProductReviews = 3,
+    WishList = 4,
+    Notification = 5
+}
